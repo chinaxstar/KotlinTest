22a7f7a92ee14cd1ab75271b507ca1c2c2d69de1
xstar <xyx_xstar@sina.cn>
2017-08-31 19:37:20 +0800

    去掉dataBinding

--- app/src/main/java/xstar/com/kotlintest/MainActivity.kt
+++ app/src/main/java/xstar/com/kotlintest/MainActivity.kt
@@ -1,40 +1,33 @@
 package xstar.com.kotlintest
 
-import android.databinding.DataBindingUtil.bind
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
-import android.view.LayoutInflater
 import android.view.View
-import com.chad.library.adapter.base.BaseQuickAdapter
-import com.chad.library.adapter.base.BaseViewHolder
+import android.widget.TextView
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.activity_main.*
 import xstar.com.kotlintest.apis.HttpMethods
 import xstar.com.kotlintest.constant.C
-import xstar.com.kotlintest.data.Response
-import xstar.com.kotlintest.databinding.ActivityMainBinding
+import xstar.com.kotlintest.recycler.BaseAdapter
+import xstar.com.kotlintest.recycler.BaseVH
 
 class MainActivity : AppCompatActivity() {
     val json = "{\"resultcode\":\"201\",\"reason\":\"请输入正确的15或18位身份证\",\"result\":null,\"error_code\":203801}"
-    val layoutManager: LinearLayoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-    lateinit var binder: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val inflate: LayoutInflater = LayoutInflater.from(this)
-        val rootview: View = inflate.inflate(R.layout.activity_main, null, false)
-        binder = bind(rootview)
-        setContentView(rootview)
-        binder.searchServerEt.addTextChangedListener(textChangeListener)
-        binder.appRecommendTv.layoutManager = layoutManager
+        setContentView(R.layout.activity_main)
+        findIDCard(search_server_et.text.toString())
+        app_recommend_tv.layoutManager=LinearLayoutManager(this)
         rvAdapter.onItemClickListener = itemClick
-        binder.appRecommendTv.adapter = rvAdapter
-        findIDCard()
-        val re = http.gson.fromJson(json, Response::class.java)
-        val data = re.result
+        app_recommend_tv.adapter=rvAdapter
+
+        search_server_et.addTextChangedListener(textChangeListener)
     }
 
     val textChangeListener = object : TextWatcher {
@@ -56,27 +49,35 @@ class MainActivity : AppCompatActivity() {
             in 6..11 -> intArrayOf(C.QQ_NUMBER)
             else -> kotlin.IntArray(0)
         }
-        rvAdapter.data.clear()
-        rvAdapter.data.addAll(result.asList())
+        rvAdapter.itemList = result.asList()
         rvAdapter.notifyDataSetChanged()
     }
 
-    val rvAdapter = object : BaseQuickAdapter<Int, BaseViewHolder>(android.R.layout.simple_list_item_1) {
-        override fun convert(helper: BaseViewHolder?, item: Int?) {
+    val rvAdapter = object : BaseAdapter<Int>() {
+        override fun onBindViewHolder(holder: BaseVH?, position: Int) {
+            holder?.itemView?.setOnClickListener { onItemClickListener?.onItemClick(this,it,position) }
+            val item = itemList!![position]
             val result = when (item) {
                 0 -> "身份证查询"
                 else -> "未查询到结果"
             }
-            helper?.setText(android.R.id.text1, result)
+            holder?.find<TextView>(android.R.id.text1)?.setText(result)
         }
+
+        init {
+            layout = android.R.layout.simple_list_item_1
+        }
+
     }
     val http = HttpMethods()
-    val itemClick = BaseQuickAdapter.OnItemClickListener { adapter, view, position ->
-        if (position == 0) findIDCard()
+    val itemClick = object:BaseAdapter.OnItemClickListener {
+        override fun onItemClick(adapter: RecyclerView.Adapter<BaseVH>, view: View, position: Int) {
+            if (position == 0) findIDCard(search_server_et.text.toString())
+        }
     }
 
-    fun findIDCard() {
-        val api = http.apiService.getIdCardInfo(C.JUHE_KEY, binder.searchServerEt.editableText.toString())
+    fun findIDCard(search: String) {
+        val api = http.apiService.getIdCardInfo(C.JUHE_KEY, search)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .doOnNext { data -> Log.e("json", http.gson.toJson(data)) }
---
+++ app/src/main/java/xstar/com/kotlintest/recycler/recycler.kt
@@ -0,0 +1,38 @@
+package xstar.com.kotlintest.recycler
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * @author: xstar
+ * @since: 2017-08-31.
+ */
+
+class BaseVH constructor(item: View) : RecyclerView.ViewHolder(item) {
+    fun <T : View> find(resId: Int): T {
+        return itemView.findViewById(resId) as T
+    }
+}
+
+abstract class BaseAdapter<T> : RecyclerView.Adapter<BaseVH>() {
+    var itemList: List<T>? = null
+    var layout: Int? = null
+    var inflate: LayoutInflater? = null
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): BaseVH {
+        if (inflate == null) inflate = LayoutInflater.from(parent?.context)
+        return BaseVH(inflate!!.inflate(android.R.layout.simple_list_item_1, parent, false))
+    }
+
+    override fun getItemCount(): Int {
+        return itemList?.size ?: 0
+    }
+
+    var onItemClickListener: OnItemClickListener? = null
+
+    open interface OnItemClickListener {
+        open fun onItemClick(adapter: RecyclerView.Adapter<BaseVH>, view: View, position: Int)
+    }
+}
+
